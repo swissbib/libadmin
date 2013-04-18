2e6f0f4d5c8d4164cbdff89ab5d3ea47ba564e68
Fabian Erni <ferni@snowflake.ch>
2013-04-18 10:55:41 +0200

    *Reformatted importer according to coding style

--- public/import/index.php
+++ public/import/index.php
@@ -3,21 +3,21 @@ require_once('Importer.class.php');
 require_once('DB.class.php');
 require_once('DataHelper.class.php');
 
-	// No warnings please
+// No warnings please
 error_reporting(E_ERROR);
-	// Start looking for files from root dir
+// Start looking for files from root dir
 chdir(dirname(dirname(__DIR__)));
 
-	// Take database credentials from ZF2 local config
-$zf2LocalConf	= include('config/autoload/local.php');
+// Take database credentials from ZF2 local config
+$zf2LocalConf = include('config/autoload/local.php');
 
 try {
-	$flush		= true;
-	$importer	= new Importer($zf2LocalConf['db'], $flush);
+	$flush = true;
+	$importer = new Importer($zf2LocalConf['db'], $flush);
 
-	$xmlFile	= '/module/Libadmin/data/tpgreen-libraries.xml';
+	$xmlFile = '/module/Libadmin/data/tpgreen-libraries.xml';
 	echo $importer->import('module/Libadmin/data/tpgreen-libraries.xml');
 
-} catch(Exception $e) {
-	die('IMPORT FAILED - ' . $e->getMessage() );
+} catch (Exception $e) {
+	die('IMPORT FAILED - ' . $e->getMessage());
 }
